ad3a2f558dd251cc934f67862ae251376215e487
Shilpa chaurasiya <142612847+shilpa84ya@users.noreply.github.com>
2024-09-22 04:52:57 +0000

    DataTypes

---
+++ 01-Basics/01_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // Treat all JS code as newer version
+
+// alert(3+3) // not supported as we are using nodejs, not browser
+
+let name = "shilpa"
+let age = 20
+let isLoggedIn = true
+let state;
+
+//Datatypes
+/* 
+1.number => 2 to the power 53
+2. bigInt
+3. string
+4.boolean => true/false
+5.undefined =>
+6.null => stand alone value
+7. symbol => for uniqueness
+ */
+
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
