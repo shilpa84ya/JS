93d98097e4b19ffb11d45433d7399d2ad0f4c52c
Shilpa chaurasiya <142612847+shilpa84ya@users.noreply.github.com>
2024-09-22 05:11:30 +0000

    Conversion Of Operator

---
+++ 01-Basics/03_conversionOperator.js
@@ -0,0 +1,30 @@
+let score = null
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInInteger = Number(score) //To convert into number datatype
+
+// console.log(typeof valueInInteger)
+// console.log(valueInInteger)
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1 false => 0
+// undefined => NaN
+// null => 0
+
+
+let isLoggedIn = 1
+let booleanValue = Boolean(isLoggedIn)
+console.log(booleanValue)
+
+// 1 => true , 0 => false
+// "" => false, "anything" => true
+
+let num = 33
+let stringValue = String(num)
+console.log(stringValue)
+console.log(typeof stringValue)
+
+// 33 => "33"
