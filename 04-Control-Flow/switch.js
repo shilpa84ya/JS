493c05ed43107f968d2ea48d56fb507eef74fb40
Shilpa chaurasiya <142612847+shilpa84ya@users.noreply.github.com>
2024-09-24 10:57:08 +0000

    Switch Cases

---
+++ 04-Control-Flow/switch.js
@@ -0,0 +1,28 @@
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 1:
+        console.log("Fabrury")
+        break;
+    case 1:
+        console.log("March")
+        break;
+    case 1:
+        console.log("April")
+        break;
+    case 1:
+        console.log("May")
+        break;
+    case 1:
+        console.log("Jun")
+        break;
+    case 1:
+        console.log("July")
+        break;
+
+    default:
+        console.log("Default case!")
+        break;
+}
