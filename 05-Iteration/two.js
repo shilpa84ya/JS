b779201bbf75223bd2326bce8b673a8a1cb701e0
Shilpa chaurasiya <142612847+shilpa84ya@users.noreply.github.com>
2024-09-24 11:25:59 +0000

    While and do-while loop

---
+++ 05-Iteration/two.js
@@ -0,0 +1,22 @@
+
+
+let index =0;
+while (index <= 10) {
+    // console.log("Value of index is ",index)
+    index = index + 2
+}
+
+
+let i =0
+const arr = ["flash","superman", "batman"]
+while(i < arr.length){
+    // console.log(`Value is ${arr[i]}`)
+    i = i+1
+}
+
+let score = 0
+do{
+    console.log(`Score is ${score}`)
+    score++
+}
+while(score<=10)
